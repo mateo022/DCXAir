005c5e7b69cec0797120fef4611d7ce0bd1df40d
mateo022 <mateoceballos022@gmail.com>
2024-04-13 13:57:09 -0500

    Creation of Journey model using base entity, this model have a list of fligths

---
+++ DCXAirAPI/DCXAirAPI.Domain/Models/Journey/Journey.cs
@@ -0,0 +1,11 @@
+﻿namespace DCXAirAPI.Domain.Models.Journey
+{
+    public class Journey : Entity.Entity
+    {
+        public string? Origin { get; set; }
+        public string? Destination { get; set; }
+        public string? Price { get; set; }
+        public List<Flight.Flight> Flights { get; set; }
+
+    }
+}
