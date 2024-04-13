120b5f067d00c1584c9eac7d96168cb2c9c95708
mateo022 <mateoceballos022@gmail.com>
2024-04-13 14:09:18 -0500

    Create of model for Flights and trasnports, create relationship between them

---
+++ DCXAirAPI/DCXAirAPI.Domain/Models/Flight/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCXAirAPI.Domain.Models.Flight
+{
+    public class Flight : Entity.Entity
+    {
+        public string? Origin { get; set; }
+        public string? Destination { get; set; }
+        public double? Price { get; set; }
+        public Guid TransportId { get; set; }
+        [ForeignKey("TransportId")]
+        public Transport.Transport? Transport { get; set; }
+    }
+}
---
+++ DCXAirAPI/DCXAirAPI.Domain/Models/Transport/Transport.cs
@@ -0,0 +1,9 @@
+﻿
+namespace DCXAirAPI.Domain.Models.Transport
+{
+    public class Transport: Entity.Entity
+    {
+        public string? FlightCarrier { get; set; }
+        public string? FlightNumber { get; set; }
+    }
+}
