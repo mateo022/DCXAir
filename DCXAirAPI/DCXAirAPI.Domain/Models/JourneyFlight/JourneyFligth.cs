119bdf238f071a3dae12e45952454ce3bf055728
mateo022 <mateoceballos022@gmail.com>
2024-04-13 15:31:21 -0500

    Creation of a weak entity since there remains a many-to-many relationship between flights and journey.

---
+++ DCXAirAPI/DCXAirAPI.Domain/Models/JourneyFlight/JourneyFligth.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCXAirAPI.Domain.Models.JourneyFlight
+{
+    public class JourneyFligth: Entity.Entity
+    {
+        public Guid FlightId { get; set; }
+        [ForeignKey("FlightId")]
+        public Flight.Flight? Flight { get; set; }
+        public Guid JourneyId { get; set; }
+        [ForeignKey("JourneyId")]
+        public Journey.Journey? Journey { get; set; }
+    }
+}
